5e4cdd6ed6fd3f1a159f6f87a68211b8188bed72
naga-aashish <aashishbunny1412@gmail.com>
2023-04-23 19:11:45 +0530

    last ommit

--- 14-04-23/code/src/app/app-routing.module.ts
+++ 14-04-23/code/src/app/app-routing.module.ts
@@ -1,13 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FirstpageComponent } from './pages/firstpage/firstpage.component';
-import { LastpageComponent } from './pages/lastpage/lastpage.component';
 
 
 
 const routes: Routes = [
   {path:"",component:FirstpageComponent},
-  {path:"login_page",component:LastpageComponent}
+  
 ];
 
 @NgModule({
